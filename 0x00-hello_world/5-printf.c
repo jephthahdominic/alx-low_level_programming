6b5d30fbda5ce760294c51e13ecce2cade639b13
St-dominic Jephthah <stdominicjephthah63@gmail.com>
2023-08-15 06:15:31 -0700

    modified the 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * Main - The main program prints out a string using printf
+ * main - The main program prints out a string using printf;
  *
- * Return - Always 0(Success)
+ * Return: Main is always (0) Success;
  */
 
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
