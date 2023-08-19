35b917212f60b1d65599177db326dd7209966a1d
St-dominic Jephthah <stdominicjephthah63@gmail.com>
2023-08-19 14:58:03 -0700

    added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - to print the size of data types
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+printf("Size of a char: %lu", sizeof());
+printf("Size of an int: %lu", sizeof());
+printf("Size of a lomg int: %lu", sizeof());
+printf("Size of a long long int: %lu", sizeof());
+printf("Size of a float: %lu", sizeof());
+return (0);
+}
