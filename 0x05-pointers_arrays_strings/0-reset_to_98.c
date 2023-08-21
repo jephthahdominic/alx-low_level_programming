dc4f067a08950de1cfda7dbb43322adb4936e6f3
St-dominic Jephthah <stdominicjephthah63@gmail.com>
2023-08-21 15:15:26 -0700

    a program to change the value of a point variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - this program resets the value of an int to 98
+ * 
+ * @n : the valuebof dereferencing pointer
+ * 
+ * return: nth
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
