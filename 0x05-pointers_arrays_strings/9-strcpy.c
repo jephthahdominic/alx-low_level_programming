45d5cb0f801aadac1f4c37fe8941fe68453d0801
St-dominic Jephthah <stdominicjephthah63@gmail.com>
2023-08-21 21:42:53 -0700

    added 9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,9 @@
 /**
  * *_strcpy - pointer to a character
  *
- *@dest - string 1 pointer 2 a char
- *@src - string 2 poimter to a char
- *Return: character (dest)
+ * @dest - string 1 pointer 2 a char
+ * @src - string 2 poimter to a char
+ * Return: character (dest)
  */
 
 char *_strcpy(char *dest, char *src)
